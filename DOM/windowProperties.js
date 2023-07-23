91b8993bc10f7c83abf07ecd93f72e942b045823
VINAYAK9669 <51508724+VINAYAK9669@users.noreply.github.com>
2023-07-23 19:38:50 +0530

    Add files via upload

---
+++ DOM/windowProperties.js
@@ -0,0 +1,13 @@
+//* Execrcise 1
+let windowInnerwidth = window.innerWidth;
+let windowInnerHeight = window.innerHeight;
+
+console.log("Inner Width" + windowInnerWidth);
+
+// How get the hostname or value typed in the address bar
+
+// Get the hostname of the current page
+var hostname = window.location.hostname;
+
+// Display the hostname in the console or elsewhere
+console.log("Hostname: " + hostname);
