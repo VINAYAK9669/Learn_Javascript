0cd3466103ba8f792f240eafd63b27c01366e921
VINAYAK9669 <51508724+VINAYAK9669@users.noreply.github.com>
2023-07-26 11:47:09 +0530

    Update NAVIGATING_A_DOM.js

--- DOM/EventHandling/NAVIGATING_A_DOM.js
+++ DOM/EventHandling/NAVIGATING_A_DOM.js
@@ -71,5 +71,102 @@ Theory:
                 This is an artificial node that can hold a group of nodes without being part of the actual DOM tree.
           
 */
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DOM Navigation</title>
+  </head>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    .container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      max-width: 800px;
+      margin: 0 auto;
+      margin-top: 2rem;
+      border: 2px solid black;
+      row-gap: 1rem;
+    }
+
+    .list li {
+      margin-left: 2rem;
+      margin-bottom: 0.3rem;
+    }
+    .list p {
+      margin-bottom: 0.8rem;
+    }
+  </style>
+  <body>
+    <div class="container">
+      <h2>HTML DOM NAVIGAION</h2>
+      <div class="header">
+        <h3>I will Teach you the DOM Navigation</h3>
+      </div>
+      <div class="list">
+        <p>I am DOM and I have below node properties</p>
+        <ol type="1">
+          <li class="test1">parentNode</li>
+          <li class="test2">childNodes[nodenumber]</li>
+          <li>firstChild</li>
+          <li>lastChild</li>
+          <li>nextSibling</li>
+          <li>previousSibling</li>
+        </ol>
+      </div>
+      <h2>HTML DOM NAVIGATION RESULT</h2>
+      <div class="list1">
+        <ol type="1">
+         
+        </ol>
+    </div>
+  </body>
+  <script>
+    // Get the reference to the <ol> element
+  const listDiv = document.querySelector(".list1");
+  const list=document.querySelector(".list");
+  const listElement1 = document.querySelector(".test1");
+  const listElement2 = document.querySelector(".test2");
+  const olElement = listDiv.querySelector("ol");
+
+  
+
+  const result1= listElement1.parentElement;
+  const listContent1= "my Parent Node is";
+
+  const result2 = listElement2.nextElementSibling.textContent;
+  // Note nextElementSibling only can able to return the text content
+  console.log(result2);
+  const listContent2= "my next Sibling Is: ";
+
+  addList(result1,listContent1);
+  addList(result2,listContent2);
+
+
+  function addList(result, text)
+  {
+    // Create a new <li> element
+  const newListItem1= document.createElement("li");
+    // Set the text content for the new list item
+    newListItem1.textContent = `${text} :${result}`;
+
+  // Append the new list item to the <ol> element
+  olElement.appendChild(newListItem1);
+
+  }
+
+  
+  </script>
+</html>
+
+
 
 
