8e082404bf00e06ac4ebd96c5c57b428a76852cd
VINAYAK9669 <51508724+VINAYAK9669@users.noreply.github.com>
2023-08-10 11:00:55 +0530

    Update Code2.js

--- Asynchronous Js/B. CallbackCodes/Code2.js
+++ Asynchronous Js/B. CallbackCodes/Code2.js
@@ -1,12 +1,18 @@
 let data;
+// Now the function fetchData accepts callback function as parameter
 function fetchData(cb){
    setTimeout(()=>{
       data={pCode:1001,pName:'Orange'};
-      cb();
+      //The call back function is called inside the setTimeout() function and after the variable data is initialized.
+      cb(); 
+      // OUTPUT: {pCode:1001,pName:'Orange'}
+       // OUTPUT:Program ends here....
+      
    },2000)
 }
  
-console.log("Program starts here....");
+console.log("Program starts here...."); // OUTPUT: Program starts here....
+// Here we will call the fetchData function and will pass the arguments
 fetchData(function(){
     console.log(data);
     console.log("Program ends here....");
