bcb94d72551e881f76f1916201fe1b0741c8a1da
VINAYAK9669 <51508724+VINAYAK9669@users.noreply.github.com>
2023-08-10 11:23:34 +0530

    Update Code3.js

--- Asynchronous Js/B. CallbackCodes/Code3.js
+++ Asynchronous Js/B. CallbackCodes/Code3.js
@@ -1,12 +1,14 @@
+// Compute the area of the square
 function cSqr(n,cb){
     setTimeout(()=>{
-        cb(n*n)
+        cb(n*n) // here calling the function and passing the arguments.
     },2000)
  }
-  
+  // Here we are calling cSqr function and passing the number and anonymous function
  cSqr(2,function(res){
-     console.log(res);
+     console.log(res); // 4
+     //nested callback
      cSqr(res,function(res1){
-         console.log(res1);
+         console.log(res1);//16
      })
  }) 
