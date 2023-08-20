6fbb0cf65327172378e52a2656d4362463778206
VINAYAK9669 <51508724+VINAYAK9669@users.noreply.github.com>
2023-08-20 12:54:36 +0530

    Create Name.js

---
+++ Import_Export/B. Named_Import_Export/Name.js
@@ -0,0 +1,18 @@
+function friendsName() {
+  console.log("Durdundi");
+  console.log("Rohan");
+  console.log("Siddu");
+  console.log("ArunKumar");
+}
+
+function relatives() {
+  console.log("Mandar");
+  console.log("Shridhar");
+}
+
+function sisters() {
+  console.log("Sheela");
+  console.log("Ashwini");
+}
+
+export { friendsName, relatives, sisters };
