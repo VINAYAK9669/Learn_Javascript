dc5b3581ca3285f9fe16407ae9550a26a3d04dd4
VINAYAK9669 <51508724+VINAYAK9669@users.noreply.github.com>
2023-08-26 16:37:51 +0530

    Create B. Character_Classes.js

---
+++ RegEx/B. Character_Classes.js
@@ -0,0 +1,32 @@
+// Shorthand Property Notation
+
+// * 1) \d - Matches any digit
+const text = "I am learning\n HTML-5\t, CSS3, JS ES6 & React_18 ?!%&";
+const pattern = /\d+/g;
+const patternMatch = text.match(pattern);
+console.log("Pattern Match d: " + patternMatch);
+
+// * 2) \D - Matches any non digit character
+const pattern1 = /\D+/g;
+const patternMatch1 = text.match(pattern1);
+console.log("Pattern Match D: " + patternMatch1);
+
+// * 3) \w - Matches any WORD-character, digits, underscores [Exludes- '-','&'..etc]
+const pattern2 = /\w+/g;
+const patternMatch2 = text.match(pattern2);
+console.log("Pattern Match w: " + patternMatch2);
+
+// * 4) \W - Matches any NON-WORD characters and digits
+const pattern3 = /\W+/g;
+const patternMatch3 = text.match(pattern3);
+console.log("Pattern Match W: " + patternMatch3);
+
+// * 5) \s - Matches any white spaces character (spaces, tabs, line-break)
+const pattern4 = /\s+/g;
+const patternMatch4 = text.match(pattern4);
+console.log("Pattern Match s: " + patternMatch4);
+
+// * 6) \S - Matches any white spaces character (exludes- spaces, tabs, line-break)
+const pattern5 = /\S+/g;
+const patternMatch5 = text.match(pattern5);
+console.log("Pattern Match S: " + patternMatch5);
